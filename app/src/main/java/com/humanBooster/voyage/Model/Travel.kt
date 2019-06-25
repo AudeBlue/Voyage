d11ca552d1ca23772468e7cf6eb3fdd3ee07038d
Gaetan <gaetan.taltavull@gmail.com>
2019-06-25 15:29:32 +0200

    Create Travel model without constructor to do

---
+++ app/src/main/java/com/humanBooster/voyage/Model/Travel.kt
@@ -0,0 +1,17 @@
+package com.humanBooster.voyage.Model
+
+import android.graphics.Bitmap
+import java.util.*
+
+data class Travel {
+
+    private var id:Int = 0
+    private var title:String = ""
+    private var date:Int = 0
+    private var place:String = ""
+    private var picture:Byte = 0
+        
+
+
+
+}
